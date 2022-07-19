1c217e7a35acdff55adba42839556fa1f3900c05
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2022-07-19 02:51:29 -0300

    Ejercicio de practica

---
+++ positive_or_negative_number.js
@@ -0,0 +1,15 @@
+numeroIf = 0;
+
+if (numeroIf < 0){
+  console.log("Es un numero negativo");
+}
+
+else if (numeroIf > 0){
+  console.log("Es un numero positivo");
+}
+else if (numeroIf == 0){
+  console.log("Es un 0");
+}
+else{
+  console.log("El valor ingresado no es valido");
+}
